e1fefe68f261da90f7a1bbae22e7ef680f0a314f
Marc <marc from kprod>
2021-02-03 20:48:02 +0100

    mod stat controller

--- src/main/java/net/kprod/firewatch/controller/StatController.java
+++ src/main/java/net/kprod/firewatch/controller/StatController.java
@@ -6,12 +6,11 @@ import net.kprod.firewatch.persistence.RepositoryEvent;
 import net.kprod.firewatch.persistence.RepositoryHistoryEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,41 +22,67 @@ public class StatController {
     @Autowired
     private RepositoryHistoryEvent rhe;
 
-    @GetMapping("/data/history")
-    public ResponseEntity<List<EventHistory>> history(@RequestParam String endpoint) {
+    @GetMapping("/data/performance/last")
+    public ResponseEntity<EventHistory> performanceLast() {
+
+        EventHistory eh = rhe.findAll().stream()
+                .sorted(Comparator.comparing(EventHistory::getId).reversed())
+                .findFirst().get();
+
+        return ResponseEntity.ok(eh);
+    }
+
+    @GetMapping("/data/performance")
+    public ResponseEntity<List<EventHistory>> performance() {
 
         List<EventHistory> list = rhe.findAll().stream()
-                .filter(e -> e.getName().equals(endpoint))
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(list);
     }
 
-    @GetMapping("/data/history/all")
-    public ResponseEntity<List<EventHistory>> history() {
+    @GetMapping("/data/performance/{site}")
+    public ResponseEntity<List<EventHistory>> performance(@PathVariable String site) {
 
         List<EventHistory> list = rhe.findAll().stream()
+                .filter(e -> e.getName().equals(site))
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(list);
     }
 
-    @GetMapping("/data/events")
-    public ResponseEntity<List<Event>> events(@RequestParam String endpoint, @RequestParam ChronoUnit unit, @RequestParam int count) {
+    @GetMapping("/data/events/last")
+    public ResponseEntity<Event> eventsLast() {
+
+        Event e = rh.findAll().stream()
+                .sorted(Comparator.comparing(Event::getId).reversed())
+                .findFirst().get();
+
+        return ResponseEntity.ok(e);
+    }
+
+    @GetMapping("/data/events/{site}")
+    public ResponseEntity<List<Event>> events(@PathVariable String site, @RequestParam String strUnit, @RequestParam int count) {
+
+        ChronoUnit unit = ChronoUnit.valueOf(strUnit);
 
         ZonedDateTime zdt = ZonedDateTime.now()
                 .minus(count, unit);
 
         List<Event> list = rh.findAll().stream()
-                .filter(e -> e.getName().equals(endpoint))
+                .filter(e -> e.getName().equals(site))
                 .filter(e -> e.getEventDate().isAfter(zdt))
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(list);
     }
 
-    @GetMapping("/data/events/all")
-    public ResponseEntity<List<Event>> events(@RequestParam ChronoUnit unit, @RequestParam int count) {
+
+
+    @GetMapping("/data/events")
+    public ResponseEntity<List<Event>> events(@RequestParam String strUnit, @RequestParam int count) {
+
+        ChronoUnit unit = ChronoUnit.valueOf(strUnit);
 
         ZonedDateTime zdt = ZonedDateTime.now()
                 .minus(count, unit);
